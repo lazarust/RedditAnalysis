9c6d87f583bc41f9a8d21eb1b96e6bc04646e584
Thomas Lazarus <lazarustda@gmail.com>
2022-11-10 19:37:01 -0600

    Fixes linter

--- apps/analyzer/views.py
+++ apps/analyzer/views.py
@@ -6,11 +6,12 @@ from apps.base.praw_utils import get_all_subs
 def index(request, *args, **kwargs):
     return render(request, "index.html")
 
+
 class IndexView(TemplateView):
-    template_name='index.html'
+    template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['subs'] = get_all_subs()
+        context = super().get_context_data(**kwargs)
+        context["subs"] = get_all_subs()
         print(context)
         return context
