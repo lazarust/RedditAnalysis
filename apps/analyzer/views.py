96462cc3155f4c976a04fc098a28c60ca42c01b4
Thomas Lazarus <lazarustda@gmail.com>
2022-11-27 02:07:16 +0000

    Sets up converting submissions to DF

--- apps/analyzer/views.py
+++ apps/analyzer/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from apps.base.praw_utils import get_all_subs
+from apps.base.praw_utils import get_post_data
 
 
 class IndexView(TemplateView):
@@ -8,3 +8,10 @@ class IndexView(TemplateView):
 
 class ChartView(TemplateView):
     template_name = "charts.html"
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        data = get_post_data(self.request.GET["subreddit-select"])
+        fig = data.plot.line(x="created_utc", y="score")
+        return context
--- apps/base/praw_utils.py
+++ apps/base/praw_utils.py
@@ -1,6 +1,26 @@
 from config.settings.base import REDDIT_CLIENT, REDDIT_SECRET, REDDIT_USERNAME
 import praw
+import pandas as pd
 
+
+def iterator_to_dataframe(iterator) -> pd.DataFrame:
+    data = []
+    for submission in iterator:
+        submission_dict = submission.__dict__
+        data.append({
+            "id": submission_dict["id"],
+            "score": submission_dict["score"],
+            "ups": submission_dict["ups"],
+            "downs": submission_dict["downs"],
+            "upvote_ratio": submission_dict["upvote_ratio"],
+            "edited": submission_dict["edited"],
+            "num_comments": submission_dict["num_comments"],
+            "created_utc": submission_dict['created_utc'],
+            "num_crossposts": submission_dict['num_crossposts'],
+            "is_video": submission_dict['is_video'],
+        })
+    
+    return pd.DataFrame(data=data)
 
 def get_all_subs():
     reddit = praw.Reddit(
@@ -14,3 +34,10 @@ def search_subs(query: str):
         client_id=REDDIT_CLIENT, client_secret=REDDIT_SECRET, user_agent=REDDIT_USERNAME
     )
     return reddit.subreddits.search(query)
+
+
+def get_post_data(subreddit:str):
+    reddit = praw.Reddit(
+        client_id=REDDIT_CLIENT, client_secret=REDDIT_SECRET, user_agent=REDDIT_USERNAME
+    )
+    return iterator_to_dataframe(reddit.subreddit(subreddit).hot(limit=1000))
